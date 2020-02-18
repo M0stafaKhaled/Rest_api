158b4a61acd0494f83684c55aae1fb912af99d0a
M0stafaKhaled <mkostafa79@gmail.com>
2020-02-18 16:14:43 +0200

    add database file

---
+++ config/Database.php
@@ -0,0 +1,32 @@
+<?php 
+class DataBase {
+
+//DB parameter 
+  private $host = 'localhost';
+  private $userName = 'root';
+  private $dataBaseName = 'blog';
+  private $password = '';
+  private $conn ; 
+//DB connect 
+
+public function conncet(){
+$this->conn = new pdo('mysql:host=' .$this->host . ';dbname ='. $this->dataBaseName , $this->username, $this->password);
+$this->conn->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION); 
+try {
+
+
+}
+  catch (PDOException $e){
+
+echo 'Connection Error:' . $e->getMessage();
+
+  }
+  
+  
+  return $this->conn; 
+}
+
+
+
+}
+
